68909502828a1412dc92478f094ec6bb40a6a1a7
HuaDeity <hello@huadeity.com>
2025-10-15 17:40:58 +0800

    Try to fix

--- flake.nix
+++ flake.nix
@@ -5,27 +5,18 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    # Shared module (just defines options, no imports)
-    sharedModules.default = ./shared.nix;
+  outputs =
+    { self, nixpkgs }:
+    {
+      # Shared module (just defines options, no imports)
+      sharedModules.default = ./shared.nix;
 
-    # Home-manager module (for home.packages)
-    homeModules.default = {
-      imports = [
-        ./home.nix
-        { config.pkgflow.manifestPackages._outputTarget = "home"; }
-      ];
+      # Home-manager module (for home.packages)
+      homeModules.default = import ./home.nix { _outputTarget = "home"; };
+
+      systemModules.default = import ./home.nix { _outputTarget = "system"; };
+
+      # Homebrew module (for Darwin homebrew.brews/casks)
+      brewModules.default = ./darwin.nix;
     };
-
-    # System module (for NixOS/Darwin environment.systemPackages)
-    systemModules.default = {
-      imports = [
-        ./home.nix
-        { config.pkgflow.manifestPackages._outputTarget = "system"; }
-      ];
-    };
-
-    # Homebrew module (for Darwin homebrew.brews/casks)
-    brewModules.default = ./darwin.nix;
-  };
 }
